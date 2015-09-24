bccd44323afe43a034b117bddb5f0c572793a670
Octavio Razo <octavio.razo.verduzco@gmail.com>
2015-09-24 14:21:32 -0500

    send php

--- send.php
+++ send.php
@@ -10,8 +10,8 @@ $mensaje = $_POST['msj'];
 
 $mensaje = "Mensaje de:". $nombre. "<br><br>". $lenguaje. " ".$nivel."<br><br>". $mensaje;
 	$encabezados = 'MIME-Version: 1.0' . "\r\n";
-	$encabezados .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-	$encabezados .= "From: $remitente\nReply-To: $remitente" ;
+	$encabezados = 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	$encabezados = "From: $remitente\nReply-To: $remitente" ;
 //for($i=0; $i<3; $i++)
 	
 	mail("octavio.razo.verduzco@gmail.com", $asunto, $mensaje, $encabezados) or die ("No se ha podido enviar tu mensaje. Ha ocurrido un error") ;
